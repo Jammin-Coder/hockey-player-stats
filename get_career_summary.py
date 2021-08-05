6016d8ad89bf17410d6fb67b8a0fa224a3151f22
JamminCoder <73239367+Jammin-Coder@users.noreply.github.com>
2021-08-05 15:30:22 -0400

    Delete get_career_summary.py

--- get_career_summary.py
+++
@@ -1,7 +0,0 @@
-from SportsStats.Players.Player import *
-
-
-stats = GetPlayerStats("https://www.hockey-reference.com/players/c/coffepa01.html")
-
-career_summary = stats.get_career_summary()
-print(career_summary)
